46ef8d858b56917cbc1fba0916083bec4eec2a54
Mahmoud <mahmoud43704@gmail.com>
2020-09-29 18:36:32 +0200

    Day4 taks edited

---
+++ Day4/test_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mahmoud/Desktop/ROS_Tech/Day4/test_ws/src
+# Build directory: /home/mahmoud/Desktop/ROS_Tech/Day4/test_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ Day4/test_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/mahmoud/Desktop/ROS_Tech/Day4/test_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
